b61ea5a43d735649970227e1fb7f62f11f29182b
khadija <60093631@udst.edu.qa>
2024-01-31 10:36:45 +0300

    Ford

--- cars.py
+++ cars.py
@@ -1,1 +1,6 @@
+import random
+
+brands = ["Toyota", "Ford", "Honda", "Chevrolet"]
+selected_brand = random.choice(brands)
+print("Selected car brand:", selected_brand)
 "print('Hello, this is a car program!')" 
